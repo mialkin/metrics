44f7df680db4e76818040c21e41e50a1037c61a3
Мялкин Алексей Николаевич <a.myalkin@tinkoff.ru>
2022-12-25 21:31:22 +0300

    Add Serilog

--- src/Metrics.Api/Program.cs
+++ src/Metrics.Api/Program.cs
@@ -1,8 +1,12 @@
 using Metrics.Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) => loggerConfiguration
+    .WriteTo.Console()
+    .ReadFrom.Configuration(hostBuilderContext.Configuration));
 
 var services = builder.Services;
 services.AddControllers();
@@ -10,8 +14,7 @@ services.AddHostedService<SampleBackgroundService>();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
 app.MapControllers();
+app.UseSerilogRequestLogging();
 
 app.Run();
