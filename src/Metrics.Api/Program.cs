312218ee45cb18e4561a6e138fbd2df054bc1d69
Мялкин Алексей Николаевич <a.myalkin@tinkoff.ru>
2022-12-26 00:21:28 +0300

    Record metrics

--- src/Metrics.Api/Program.cs
+++ src/Metrics.Api/Program.cs
@@ -1,7 +1,17 @@
-using Metrics.Api;
+using System;
+using System.Diagnostics.Metrics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 using Serilog;
+
+var resource = ResourceBuilder.CreateDefault().AddService(serviceName: "Sample metrics service");
+var meterName = "Aleksei's meter";
+var meter = new Meter(name: meterName, version: "1.0"); // Think of a meter as of a container for all of your metrics
+var counter = meter.CreateCounter<long>(name: "Requests");
+var histogram = meter.CreateHistogram<long>(name: "RequestDuration");
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) => loggerConfiguration
@@ -10,11 +20,23 @@ builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) => loggerConfi
 
 var services = builder.Services;
 services.AddControllers();
-services.AddHostedService<SampleBackgroundService>();
+// services.AddHostedService<SampleBackgroundService>();
+
+services.AddOpenTelemetry().WithMetrics(x =>
+{
+    x.SetResourceBuilder(resource);
+    x.AddConsoleExporter();
+    x.AddMeter(meterName);
+}).StartWithHost();
 
 var app = builder.Build();
 
-app.MapControllers();
-app.UseSerilogRequestLogging();
+app.MapGet("/", () =>
+{
+    counter.Add(1);
+    histogram.Record(Random.Shared.Next(0, 100));
+});
 
-app.Run();
+app.MapControllers();
+
+app.Run(); // Run app without debugger to see output in console
