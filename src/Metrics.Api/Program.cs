039d444594c1a4540160b26e5c3c48a20bcd3ab0
Мялкин Алексей Николаевич <a.myalkin@tinkoff.ru>
2022-12-25 21:26:34 +0300

    Add background service

--- src/Metrics.Api/Program.cs
+++ src/Metrics.Api/Program.cs
@@ -1,11 +1,12 @@
+using Metrics.Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
-
 services.AddControllers();
+services.AddHostedService<SampleBackgroundService>();
 
 var app = builder.Build();
 
---
+++ src/Metrics.Api/SampleBackgroundService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Metrics.Api;
+
+public class SampleBackgroundService : BackgroundService
+{
+    private readonly ILogger<SampleBackgroundService> _logger;
+
+    public SampleBackgroundService(ILogger<SampleBackgroundService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Processing data. Current date: {Date}", DateTime.UtcNow);
+
+            await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
+        }
+    }
+}
